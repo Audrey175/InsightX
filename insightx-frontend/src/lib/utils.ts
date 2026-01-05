a8cbde2cb56e3d93081db010656e7de7e11d8161
Audrey175 <s3975091@rmit.edu.vn>
2026-01-05 16:21:00 +0700

    feat: add reconstruction for h5 format

--- insightx-frontend/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-console.log("DEBUG: utils.ts IS LOADED!");
-
-export function cn(...classes: (string | undefined | null | false)[]) {
-  console.log("DEBUG: cn() function called with:", classes);
-  return classes.filter(Boolean).join(" ");
-}
